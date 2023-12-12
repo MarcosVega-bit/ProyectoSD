b2b4438d23a43e576a64310bcb8cc9ef25779ab4
MarcosVega-bit <54427661+MarcosVega-bit@users.noreply.github.com>
2023-12-12 17:21:01 -0600

    Update MWf.py

--- MWf.py
+++ MWf.py
@@ -172,7 +172,7 @@ def cliente(conn, addr):
         #print(f'Mensaje recibido de {addr}: {received_message}')
         
         # Almacenar mensaje recibido en un archivo
-        with open(f"/home/eduardo/msgs.txt", "a") as file:
+        with open(f"/home/marcos_25/msgs.txt", "a") as file:
             file.write(f"[Recibido] {time.strftime('%Y-%m-%d_%H:%M:%S')} - {received_message}\n")
         
         # Enviar un mensaje de confirmación al cliente
@@ -202,7 +202,7 @@ def mensaje(server_ip, server_port, message):
         print(f"Mensaje enviado a {server_ip}:{server_port}: {mt}")
         
         # Almacenar mensaje enviado en un archivo
-        with open(f"/home/eduardo/msgs.txt", "a") as file:
+        with open(f"/home/marcos_25/msgs.txt", "a") as file:
             file.write(f"[Enviado] {t} - {message}\n")
         
         response = s.recv(1024)
@@ -210,7 +210,7 @@ def mensaje(server_ip, server_port, message):
         print(f"Respuesta del servidor {server_ip}:{server_port}: {decoded_response}")
         
         # Almacenar mensaje de confirmación recibido en un archivo
-        with open(f"/home/eduardo/msgs.txt", "a") as file:
+        with open(f"/home/marcos_25/msgs.txt", "a") as file:
             file.write(f"[Recibido] {time.strftime('%Y-%m-%d_%H:%M:%S')} - {decoded_response}\n")
 
 #if __name__ == "__main__":
