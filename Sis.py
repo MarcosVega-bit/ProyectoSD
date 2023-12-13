ce67d3a1ee52dae37750e857fd8f3f8899b987cb
MarcosVega-bit <54427661+MarcosVega-bit@users.noreply.github.com>
2023-12-12 21:06:30 -0600

    Update Sis.py

--- Sis.py
+++ Sis.py
@@ -1,50 +1,57 @@
 import socket
 import threading
 import sqlite3
-import random
+import time
 import MWf
-from MWf import EleccionTokenRing 
 
-bd = sqlite3.connect('/home/marcos_25/base.sqlite')
+bd = sqlite3.connect('/home/eduardo/base.sqlite', check_same_thread=False)
 cur = bd.cursor()
 idP = 1
 idC = 1
 
-if __name__ == "__main__":
-    # Configuración de los servidores en cada máquina virtual
-    hosts = [
-        "192.168.159.130",
-        "192.168.159.134",
-        "192.168.153.130",
-        "192.168.153.131"
-    ]
-    port = 12345  # Puerto para la comunicación entre las máquinas
-
-    maestro = 0 # Bandera que indica que nodo es el maestro
-
-    espera = True # Bandera que espera respuesta
-    # Crear una instancia de EleccionTokenRing con los hosts y el puerto
-    eleccion_token_ring = EleccionTokenRing(hosts, port)
-
-    # Iniciar un hilo para manejar la elección y el token ring
-    eleccion_thread = threading.Thread(target=eleccion_token_ring.iniciar_anillo)
-    eleccion_thread.start()
-
+# Configuración de los servidores en cada máquina virtual
+hosts = [
+    "192.168.153.128",
+    "192.168.153.129",
+    "192.168.153.130",
+    "192.168.153.131"
+]
+port = [      # Puerto para la comunicación entre las máquinas
+    1111,
+    2222,
+    3333,
+    4444
+]
     
-    cur.execute('DROP TABLE IF EXISTS PRODUCTO')
+names = [    # Nombres dehost de las máquinas
+    "VM1",
+    "VM2",
+    "VM3",
+    "VM4"
+]
+# Variables para la detección de fallas del maestro
+maestro = False
+temporizador = 10
+        
+if __name__ == "__main__":
+    # Borrado de tablas
+    cur.execute('DROP TABLE IF EXISTS ARTICULO')
     cur.execute('DROP TABLE IF EXISTS CLIENTE')
     cur.execute('DROP TABLE IF EXISTS INVENTARIO')
+    cur.execute('DROP TABLE IF EXISTS ENVIO')
+    
     # Creacion de tablas
-    cur.execute('CREATE TABLE PRODUCTO (idProducto INTEGER, nombre TEXT, total INTEGER)')
+    cur.execute('CREATE TABLE ARTICULO (idArticulo INTEGER, nombre TEXT, total INTEGER)')
     cur.execute('CREATE TABLE CLIENTE (idCliente INTEGER, nombre TEXT, apPaterno TEXT, apMaterno TEXT)')
-    cur.execute('CREATE TABLE INVENTARIO (idSucursal, producto INTEGER, cantidad INTEGER)')
+    cur.execute('CREATE TABLE INVENTARIO (idSucursal INTEGER, idArticulo INTEGER, cantidad INTEGER)')
+    cur.execute('CREATE TABLE ENVIO (idArticulo INTEGER, idSucursal INTEGER, idCliente INTEGER)')
 
-    #cur.execute('INSERT INTO PRODUCTOS (idProducto, nombre) VALUES (?, ?)', ('My Way', 15))
-    cur.execute('INSERT INTO PRODUCTO (idProducto, nombre, total) VALUES (?, ?, ?)',(idP,'Zapatos', 20))
+    #cur.execute('INSERT INTO ARTICULO (idArticulo, nombre) VALUES (?, ?)', ('My Way', 15))
+    cur.execute('INSERT INTO ARTICULO (idArticulo, nombre, total) VALUES (?, ?, ?)',(idP,'Zapatos', 20))
     idP += 1
-    cur.execute('INSERT INTO PRODUCTO (idProducto, nombre, total) VALUES (?, ?, ?)',(idP,'Gorra', 16))
+    cur.execute('INSERT INTO ARTICULO (idArticulo, nombre, total) VALUES (?, ?, ?)',(idP,'Gorra', 16))
     idP += 1
-    cur.execute('INSERT INTO PRODUCTO (idProducto, nombre, total) VALUES (?, ?, ?)',(idP,'Hoodie', 12))
+    cur.execute('INSERT INTO ARTICULO (idArticulo, nombre, total) VALUES (?, ?, ?)',(idP,'Hoodie', 12))
     idP += 1
     cur.execute('INSERT INTO CLIENTE (idCliente, nombre, apPaterno, apMaterno) VALUES (?,?,?,?)',(idC,'Brayan','Ambriz','Zuloaga'))
     idC += 1
@@ -55,14 +62,11 @@ if __name__ == "__main__":
 
     i = 1
     j = -1
-
-    ipl = ''
-    ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    hn = socket.gethostname()
-    ipl = socket.gethostbyname(hn)
     
+    hn = socket.gethostname()
+    # Llenado de la tabla de inventario local de la sucursal
     while (i < idP):
-        cur.execute('SELECT total FROM PRODUCTO WHERE idProducto = ?',(i, ))
+        cur.execute('SELECT total FROM ARTICULO WHERE idArticulo = ?',(i, ))
         a = cur.fetchone()
         n = a[0]
         m = len(hosts)
@@ -70,91 +74,128 @@ if __name__ == "__main__":
         r = n % m
         for x in range(r):
             t[x] += 1
-        if (ipl == hosts[0]):
+        if (hn == names[0]):
             j = 1
-        elif (ipl == hosts[1]):
+        elif (hn == names[1]):
             j = 2
-        elif (ipl == hosts[2]):
+        elif (hn == names[2]):
             j = 3
-        elif (ipl == hosts[3]):
+        elif (hn == names[3]):
             j = 4
-        cur.execute('INSERT INTO INVENTARIO (idSucursal, producto, cantidad) VALUES (?,?,?)',(j,i,t[j-1]))
+        cur.execute('INSERT INTO INVENTARIO (idSucursal, idArticulo, cantidad) VALUES (?,?,?)',(j,i,t[j-1]))
         i += 1
     bd.commit()
-    #conn.close()
+
     
-    # Iniciar los servidores en cada máquina virtual
-    for host in hosts:
-        server_thread = threading.Thread(target=MWf.servidor, args=(host, port))
-        server_thread.start()
+    # Iniciar el servidor dependiendo la máquina virtual
+    if (hn == names[0]):
+        vm1 = threading.Thread(target=MWf.servidor, args=(hosts[0], port[0]))
+        vm1.start()
+    elif (hn == names[1]):
+        vm2 = threading.Thread(target=MWf.servidor, args=(hosts[1], port[1]))
+        vm2.start()
+    elif (hn == names[2]):
+        vm3 = threading.Thread(target=MWf.servidor, args=(hosts[2], port[2]))
+        vm3.start()
+    elif (hn == names[3]):
+        vm4 = threading.Thread(target=MWf.servidor, args=(hosts[3], port[3]))
+        vm4.start()
     
     while True:
+         if not maestro:
+            print("¡Alerta! El maestro actual ha fallado. Iniciando proceso de elección de maestro...")
+            # Lógica para elegir el nuevo maestro, basada en el hostname en este ejemplo
+            nuevo_maestro = names[0]  # Elige siempre el primer nodo como nuevo maestro (ajusta según necesites)
+            if nuevo_maestro == socket.gethostname():
+                maestro = True
+                print(f"Soy el nuevo maestro en {socket.gethostname()}")
+            else:
+                print(f"{nuevo_maestro} se convirtió en el nuevo maestro.")
         # Menu de seleccion
         print("\nBienvenido al sistema de inventarios, que deseas hacer?:")
         print("\n1. Consultar clientes")
         print("\n2. Agregar nuevo cliente")
         print("\n3. Comprar articulo")
-        print("\n4. Agregar articulo\n")
+        print("\n4. Agregar articulo")
+        print("\n5. Consultar envios\n")
 
         choice = input("Ingrese el número de opción correspondiente o '0' para salir: ")
         if choice == '0':
             break
-        eleccion_token_ring.manejar_token()
         try:
-            if choice == '1':
+            if choice == '1':    # 1. Consultar clientes
                 cur.execute('SELECT * FROM CLIENTE')
                 print("(idCliente, nombre, apPaterno, apMaterno)")
                 for fila in cur:
                     print(fila)
-            elif choice == '2':
+                    
+            elif choice == '2':    # 2. Agregar nuevo cliente
                 n = input("\nCuál es el nombre del cliente?: ")
                 p = input("\nCuál es el apellido paterno del cliente?: ")
                 m = input("\nCuál es el apellido materno del cliente?: ")
-                cur.execute('INSERT INTO CLIENTE (idCliente, nombre, apPaterno, apMaterno) VALUES (?,?,?,?)', (idC, n, p, m))
-                idC += 1
-                bd.commit()
-                 
-                print("Se agrego el producto ",n," "," ",p," ",m," correctamente")
-                #for host in hosts:
-                #    mensaje(host,port,"cliente")
-                #    mensaje(host,port,nom)
-                #    mensaje(host,port,apPat)
-                #    mensaje(host,port,apMat)
-                
-                #espera = True
-            elif choice == '3':
-               print("")
-            elif choice == '4':
+                cur.execute('SELECT COUNT(*) FROM CLIENTE')
+                id = cur.fetchone()[0]
+                id += 1
+                ids = str(id)
+                msj = "cliente "+ids+" "+n+" "+p+" "+m
+                MWf.mensaje(hosts[0],port[0],msj)
+                MWf.mensaje(hosts[1],port[1],msj)
+                MWf.mensaje(hosts[2],port[2],msj)
+                MWf.mensaje(hosts[3],port[3],msj)
+        
+            elif choice == '3':    # 3. Comprar articulo
+                print("\nEste es el inventario del nodo ",MWf.getSucId(hn),": ")
+                cur.execute('SELECT * FROM INVENTARIO')
+                print("(idSucursal, idArticulo, cantidad)")
+                for fila in cur:
+                    print(fila)
+                print("Donde los idArticulo corresponden a: ")
+                cur.execute('SELECT idArticulo, nombre FROM ARTICULO')
+                print("(idArticulo, nombre)")
+                for fila in cur:
+                    print(fila)
+                idp = input("\nCuál es el ID del ARTICULO que deseas comprar?: ")
+                c = input("\nQué cantidad de ARTICULO deseas comprar?: ")
+                idc = input("\nCuál es el su ID de cliente?: ")
+                idps = str(idp)
+                cs = str(c)
+                idcs = str(idc)
+                msj = "compra "+idps+" "+cs+" "+hn+" "+idcs
+                MWf.mensaje(hosts[0],port[0],msj)
+                MWf.mensaje(hosts[1],port[1],msj)
+                MWf.mensaje(hosts[2],port[2],msj)
+                MWf.mensaje(hosts[3],port[3],msj)
+            
+            elif choice == '4':    # 4. Agregar articulo
                 a = input("\nCuál es el nombre del nuevo articulo?: ")
-                p = input("\nCuál es la cantidad total del producto??: ")
-                cur.execute('INSERT INTO PRODUCTO (idProducto, nombre, total) VALUES (?,?,?)',(idP,a,p))
-                idP += 1
-
-                x = 0
-                n = int(p)
-                m = len(hosts)
-                t = [n//m]*m
-                r = n % m
-                for z in range(r):
-                    t[z] += 1
-                cur.execute('INSERT INTO INVENTARIO (idSucursal, producto, cantidad) VALUES (?,?,?)',(j,idP-1,t[j-1]))
-                bd.commit()
-                print("Se agrego el producto ",a," correctamente.")
+                p = input("\nCuál es la cantidad total del articulo?: ")
+                cur.execute('SELECT COUNT(*) FROM ARTICULO')
+                id = cur.fetchone()[0]
+                id += 1
+                ids = str(id)
+                msj = "articulo "+ids+" "+a+" "+p
+                MWf.mensaje(hosts[0],port[0],msj)
+                MWf.mensaje(hosts[1],port[1],msj)
+                MWf.mensaje(hosts[2],port[2],msj)
+                MWf.mensaje(hosts[3],port[3],msj)
                 
             elif choice == '5':
-                cur.execute('SELECT * FROM INVENTARIO')
-                print("(idSucursal, producto, cantidad)")
+                cur.execute('SELECT * FROM ENVIO')
+                print("(idArticulo, idSucursal, idCliente)")
                 for fila in cur:
                     print(fila)
+                    
             elif choice == '6':
-                cur.execute('SELECT * FROM PRODUCTO')
-                print("(idProducto, nombre, total)")
+                cur.execute('SELECT * FROM INVENTARIO')
+                print("(idSucursal, idArticulo, cantidad)")
                 for fila in cur:
                     print(fila)
+                    
+            elif choice == '7':
+                cur.execute('SELECT * FROM ARTICULO')
+                print("(idArticulo, nombre, total)")
+                for fila in cur:
+                    print(fila)
+                    
         except ValueError:
             print("Entrada inválida. Ingrese un número válido o '0' para salir.")
-
-# Espera a que todos los hilos hayan terminado
-for thread in threading.enumerate():
-    if thread != threading.current_thread():
-        thread.join()
